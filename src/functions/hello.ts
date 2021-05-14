0376ea98c36446c69a471ef7faf4bc8091d59b9c
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-05-14 17:07:53 -0300

    declaração da função hello

---
+++ src/functions/hello.ts
@@ -0,0 +1,12 @@
+
+export const handle = (event) => {
+  return {
+    statusCode: 201,
+    body: {
+      message: 'Hello World Ignite Serverless'
+    },
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+}
