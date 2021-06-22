a3518e483a11acd4193007556785936cf9a8d114
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-22 20:33:11 -0300

    função para compilar certificado

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,9 +1,27 @@
+import chromium from 'chrome-aws-lambda';
+import path from 'path';
+import fs from 'fs';
+import handlebars from 'handlebars';
+import dayjs from 'dayjs';
 import { document } from '../utils/dynamodbClient';
 
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
+}
+
+interface ITemplateParams { 
+  id: string;
+  name: string;
+  grade: string;
+  date: string;
+  medal: string;
+}
+const compileTemplate = async (data: ITemplateParams ) => {
+  const filePath = path.join(process.cwd(), 'src', 'templates', 'certificate.hbs');
+  const html = fs.readFileSync(filePath, 'utf-8');
+  return handlebars.compile(html)(data);
 }
 
 export const handle = async (event) => {
@@ -13,6 +31,17 @@ export const handle = async (event) => {
     TableName: 'users_certificates',
     Item: {id, name, grade}
   }).promise();
+
+  const medalPath = path.join(process.cwd(),'src', 'templates', 'selo.png');
+  const medal = fs.readFileSync(medalPath, 'base64');
+
+  const content = await compileTemplate({
+    id,
+    name,
+    grade,
+    date: dayjs().format("DD/MM/YYYY"),
+    medal,
+  });
 
   return { 
     statusCode: 201,
